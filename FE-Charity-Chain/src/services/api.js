6e5e17c43c5315bfbf2bc6a4b51c17bb0c49b88a
hieuvovan0103 <govovanhieu@gmail.com>
2026-03-23 06:33:38 +0000

    Edit api.js

--- FE-Charity-Chain/src/services/api.js
+++ FE-Charity-Chain/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://20.191.147.36:8073',
+  baseURL: import.meta.env.VITE_API_URL || 'https://20.191.147.36:8073',
   headers: {
     'Content-Type': 'application/json',
   },
